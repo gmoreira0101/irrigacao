6d48298fe5bb8c002c9b46422b4ba1210b80a710
Guilherme Moreira <gmoreira0101@gmail.com>
2024-11-03 21:55:33 -0300

    Primeiro commit com as dependencias

---
+++ src/main/java/br/com/fiap/irrigacao/IrrigacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.irrigacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrrigacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrrigacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/irrigacao/IrrigacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.irrigacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrrigacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
